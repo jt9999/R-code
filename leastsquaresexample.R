d0c8ba0fa71811ebf78c18d180ffe528514ee03b
jt9999 <114378060+jt9999@users.noreply.github.com>
2022-09-25 21:46:23 -0700

    Delete leastsquaresexample.R

--- leastsquaresexample.R
+++
@@ -1,9 +0,0 @@
-#least squares
-linearreg<-lm(cfseal$stomach~cfseal$weight)
-print(linearreg)
-summary(linearreg)
-plot(cfseal$weight,cfseal$stomach,xlim=c(0,100),ylim=c(0,1500),xlab="Seal Weight",ylab="Stomach Weight",main="Seal Weight vs Seal Stomach Weight")
-abline(lm(cfseal$stomach~cfseal$weight),cex=1.3,pch=16)
-
-plot(cfseal$weight,cfseal$stomach,xlab="Seal Weight",ylab="Stomach Weight",main="Seal Weight vs Seal Stomach Weight")
-abline(lm(cfseal$stomach~cfseal$weight),cex=1.3,pch=16)
